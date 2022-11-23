aa895b25e6bfa05329920d6606351b0b65485df8
afser7710 <afser.uddin@bengalmobileqa.com>
2022-11-23 18:31:52 +0600

    working on

--- app/src/main/java/com/example/bookingbooth/ui/HomeActivity.kt
+++ app/src/main/java/com/example/bookingbooth/ui/HomeActivity.kt
@@ -2,11 +2,36 @@ package com.example.bookingbooth.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
+import com.example.bookingbooth.ui.fragments.HomeFragment
 import com.rookie.bookingbooth.R
+import kotlinx.android.synthetic.main.activity_home.*
 
 class HomeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
+
+        val homeFragment = HomeFragment()
+        val favFragment = HomeFragment()
+        val profileFragment = HomeFragment()
+
+        setCurrentFragment(homeFragment)
+
+        bottomNavigationView.setOnNavigationItemSelectedListener {
+            when (it.itemId) {
+                R.id.home -> setCurrentFragment(homeFragment)
+                R.id.favorite -> setCurrentFragment(favFragment)
+                R.id.profile -> setCurrentFragment(profileFragment)
+                R.id.message -> setCurrentFragment(profileFragment)
+            }
+            true
+        }
     }
+
+    private fun setCurrentFragment(fragment: Fragment) =
+        supportFragmentManager.beginTransaction().apply {
+            replace(R.id.flFragment, fragment)
+            commit()
+        }
 }
